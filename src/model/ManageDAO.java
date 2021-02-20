d59be3e870fcac709a53a982ca7c2c5ea1062584
William Fonseca <santifon15@gmail.com>
2021-02-19 22:52:57 -0500

    add de csv file

--- src/model/ManageDAO.java
+++ src/model/ManageDAO.java
@@ -1,6 +1,19 @@
 package model;
 
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class ManageDAO {
+
+  private File file;
+  private ObjectInputStream in;
+  private ObjectOutputStream ou;
+  private FileReader fr;
+  private BufferedReader br;
+
   public ManageDAO() {
 
   }
